b1cda7431111ed099e1daa942856f89ecbeb97e5
Fathalla <fathalla.ebrahem22@gmail.com>
2026-06-22 01:13:48 +0300

    add cloudgate binary to formula

--- Formula/aws-eks-login.rb
+++ Formula/aws-eks-login.rb
@@ -1,17 +1,19 @@
 class AwsEksLogin < Formula
   desc "AWS SSO login and EKS IP whitelisting CLI tools"
   homepage "https://github.com/FathAllaTechOps/aws-eks-login"
-  url "https://github.com/FathAllaTechOps/aws-eks-login/archive/v1.3.0.tar.gz"
-  sha256 "4b156e21a5cbb3adb7d1599b00a4d4a84e3ab683195e02e5337600c63243a891"
-  version "v1.3.0"
+  url "https://github.com/FathAllaTechOps/aws-eks-login/archive/v1.2.0.tar.gz"
+  sha256 "2d94feab293ceb5778650f750b02aaf6cf54eeab41875cb294a80ed0452857e5"
+  version "v1.2.0"
 
   def install
     bin.install "bin/aws-login.sh" => "aws-login"
     bin.install "bin/eks-allowip.sh" => "eks-allowip"
+    bin.install "bin/cloudgate.sh" => "cloudgate"
   end
 
   test do
     system "#{bin}/aws-login", "--help"
     system "#{bin}/eks-allowip", "--help"
+    system "#{bin}/cloudgate", "--help"
   end
 end
