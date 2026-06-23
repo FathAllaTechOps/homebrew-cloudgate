e3862355ec4499cff707f623cbbe4fc65afa10d9
Fathalla <fathalla.ebrahem22@gmail.com>
2026-06-23 11:07:23 +0300

    rename formula from aws-eks-login to cloudgate

---
+++ Formula/cloudgate.rb
@@ -0,0 +1,19 @@
+class Cloudgate < Formula
+  desc "AWS authentication and EKS IP whitelisting CLI toolkit"
+  homepage "https://github.com/FathAllaTechOps/aws-eks-login"
+  url "https://github.com/FathAllaTechOps/aws-eks-login/archive/v2.1.0.tar.gz"
+  sha256 "7ec1db8a23f6eb6d74d9da5c076700ebd0205fb324e589f6ac88ca097048e9c5"
+  version "v2.1.0"
+
+  def install
+    bin.install "bin/aws-login.sh" => "aws-login"
+    bin.install "bin/eks-allowip.sh" => "eks-allowip"
+    bin.install "bin/cloudgate.sh" => "cloudgate"
+  end
+
+  test do
+    system "#{bin}/cloudgate", "--help"
+    system "#{bin}/aws-login", "--help"
+    system "#{bin}/eks-allowip", "--help"
+  end
+end
