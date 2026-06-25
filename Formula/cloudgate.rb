6ae3b088b460341d29ef161078f1b4361870ecaa
Fathalla <fathalla.ebrahem22@gmail.com>
2026-06-26 01:05:22 +0300

    fix SHA256 after repo rename from aws-eks-login to cloudgate

--- Formula/cloudgate.rb
+++ Formula/cloudgate.rb
@@ -2,7 +2,7 @@ class Cloudgate < Formula
   desc "AWS authentication and EKS IP whitelisting CLI toolkit"
   homepage "https://github.com/FathAllaTechOps/cloudgate"
   url "https://github.com/FathAllaTechOps/cloudgate/archive/v2.1.0.tar.gz"
-  sha256 "7ec1db8a23f6eb6d74d9da5c076700ebd0205fb324e589f6ac88ca097048e9c5"
+  sha256 "bbf822e3d150991241a54994a5251496481df6f8e1f73500e44236d53bb642b4"
   version "v2.1.0"
 
   def install
